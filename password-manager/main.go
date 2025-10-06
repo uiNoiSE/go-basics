3dbd6619bf17ca89e8597fcc11a137d6a7b32187
uiNoiSE <uiNoiSE@gmail.com>
2025-10-06 16:00:17 +0300

    feat(password-manager):
    Ознакомился с struct

---
+++ password-manager/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
+func main() {
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите URL")
+
+	myAccount := account{
+		login:    login,
+		password: password,
+		url:      url,
+	}
+
+	outputPassword(myAccount)
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt, ": ")
+
+	var res string
+	fmt.Scan(&res)
+
+	return res
+}
+
+func outputPassword(acc account) {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
